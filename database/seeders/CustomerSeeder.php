b747c0dddb7576a72c3aa8339d250a2a84982194
unknown <iliyakarimi250@gmail.com>
2025-07-19 12:46:47 +0330

    update

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::first() ?? User::factory()->create();
+
+        Customer::factory()->count(10)->create([
+            'user_id' => $user->id,
+        ]);
+    }
+}
