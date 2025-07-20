8e6b1e39e91195a575d61135fc907f3a69b3a19f
unknown <iliyakarimi250@gmail.com>
2025-07-20 19:32:25 +0330

    update

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,23 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth'])->group(function(){
-    Route::resource('/customers', CustomerController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    route::resource('customers', CustomerController::class);
 });
+
+
+require __DIR__.'/auth.php';
